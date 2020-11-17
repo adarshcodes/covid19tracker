cb8a758f8233389f1c9fd5ea5c13c284a39ecf2b
Adarsh Pratap Singh <adarsh.lrnr@gmail.com>
2020-11-17 15:08:29 +0530

    completed-global-stats-markup and styles

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+// HamMenu Animation //
+
 const hamMenu = document.querySelector('.menu-icons');
 
 const ham1 = document.querySelector('.line1');
@@ -12,3 +14,40 @@ hamMenu.addEventListener('click', function () {
     ham2.classList.toggle('ham2style');
     ham3.classList.toggle('ham3style');
 });
+
+//End of HamMenu Animation //
+
+
+// Typing Effect? //
+// import Typewriter from 'typewriter-effect/dist/core';
+
+let typeMain = document.getElementById('type-main');
+let typeSub = document.getElementById('type-sub');
+
+let typewriterMain = new Typewriter(typeMain, {
+    // loop: true
+    delay: 200
+});
+
+let typewriterSub = new Typewriter(typeSub, {
+    delay: 200
+})
+
+typewriterMain.typeString('Coronavirus')
+    .pauseFor(2500)
+    // .deleteAll()
+    // .typeString('Stay Home...')
+    // .pauseFor(2500)
+    // .deleteAll(7)
+    // .typeString('Save <strong>Yourself!</strong>')
+    // .pauseFor(2500)
+    // .deleteChars(9)
+    // .typeString('<strong>The Word!</strong>')
+    // .pauseFor(2500)
+    .start()
+
+typewriterSub.typeString('Live tracker')
+.pauseFor(5000)
+.start()
+
+// End of Typing Effect //
