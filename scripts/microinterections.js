30c0049a8595f827a5d5a4da3c70badddf6e5c01
Adarsh Pratap Singh <adarsh.lrnr@gmail.com>
2020-12-01 23:37:26 +0530

    completed markup and styling for prevention section

--- scripts/microinterections.js
+++ scripts/microinterections.js
@@ -94,3 +94,34 @@ let swiper = new Swiper('.blog-slider', {
 });
 
 // ########### End of Symptoms Card Effect ##############//
+
+// ############ Prevention Timeline ##############//
+
+var items = document.querySelectorAll(".section-prevention li");
+
+function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+        if (isElementInViewport(items[i])) {
+            if (!items[i].classList.contains("in-view")) {
+                items[i].classList.add("in-view");
+            }
+        } else if (items[i].classList.contains("in-view")) {
+            items[i].classList.remove("in-view");
+        }
+    }
+}
+
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
+
+// ############ End Prevention Timeline ##############//
